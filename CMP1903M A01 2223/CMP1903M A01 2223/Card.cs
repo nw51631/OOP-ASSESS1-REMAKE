15cf776d61739d20adc2142056b45d03ade80432
nw51631 <127346229+nw51631@users.noreply.github.com>
2023-03-09 14:55:16 +0000

    Update Card.cs

--- CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace CMP1903M_A01_2223
 {
-    class Card
+    public class Card
     {
-        //Base for the Card class.
-        //Value: numbers 1 - 13
-        //Suit: numbers 1 - 4
-        //The 'set' methods for these properties could have some validation
+        
         public int Value { get; set; }
         public int Suit { get; set; }
+    }
+    public class Suit
+    
+    {
+        public string Name { get; set; }
+        public int SortOrder { get; set; }
     }
 }
